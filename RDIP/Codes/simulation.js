bedcf42d95281230adfbe683f80aa0af0b0149a8
Ritu Kumari <ritu98cg@gmail.com>
2020-09-13 20:20:08 +0530

    Created

---
+++ RDIP/Codes/simulation.js
@@ -0,0 +1,3 @@
+function language(){
+    document.getElementById("intro").innerHTML="<b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b><br>(select the buttons in proper order)";
+}
